ea67777a61d1229ec6804d600d4645390a4c0a83
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-01-12 12:00:54 -0500

    c tranquille ca fais du sens

--- CompeJeux_Equipe_E/Assets/Assets/Player/InventoryManager.cs
+++ CompeJeux_Equipe_E/Assets/Assets/Player/InventoryManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InventoryManager : MonoBehaviour
 {
     public Dictionary<string,int> inventory = new Dictionary<string,int>();
+    string Selected = "";
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory.Add("Petunia", 0);
+        inventory.Add("Jonquiere", 0);
+        inventory.Add("Tulipe", 0);
     }
 
     // Update is called once per frame
@@ -18,7 +22,19 @@ public class InventoryManager : MonoBehaviour
     }
     void OnPlant()
     {
-
+        inventory.TryGetValue(Selected, out int number);
+        if (number < 3)
+            print("cant plant");
     }
-    void OnSelectPlant()
+    void OnSelectPlant(InputValue input) 
+    {
+        if(input == null)
+            return;
+        if ((int)input.Get() == 1)
+            Selected = "Petunia";
+        if ((int)input.Get() == 2)
+            Selected = "Jonquiere";
+        if ((int)input.Get() == 3)
+            Selected = "Tulipe";
+    }
 }
