449c093e2b90357eda0d9bd5714335a983428fd6
Jake <jgarant2017@outlook.com>
2023-01-12 07:11:03 -0500

    UwU

--- CompeJeux_Equipe_E/Assets/Scripts/WeedGeneration.cs
+++ CompeJeux_Equipe_E/Assets/Scripts/WeedGeneration.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class WeedGeneration : MonoBehaviour
 {
-    public GameObject gridSystem;
+    public Texture2D heatMap;
+    public GameObject weedAsset;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,22 @@ public class WeedGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InvokeRepeating("GenerateNewWeed", 1f, 1f);
+    }
+
+    void GenerateNewWeed()
+    {
+        while (true)
+        {
+            Vector2Int pos = new Vector2Int(Random.Range(0, 56), Random.Range(0, 56));
+            if (heatMap.GetPixel(pos.x, pos.y).r > Random.Range(0, 100))
+            {
+                Instantiate(weedAsset, new Vector3(pos.x * 0.32f - 9.6f, pos.y * 0.32f - 9.6f, 0f), Quaternion.identity);            }
+        }
+    }
+
+    void GenerateAdjacentWeed()
+    {
+
     }
 }
