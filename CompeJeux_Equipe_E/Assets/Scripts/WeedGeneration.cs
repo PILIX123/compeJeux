4e45a2ef00c33f7e46d3e36df8c37138b0b98091
Jake <jgarant2017@outlook.com>
2023-01-12 08:02:17 -0500

    OwO

--- CompeJeux_Equipe_E/Assets/Scripts/WeedGeneration.cs
+++ CompeJeux_Equipe_E/Assets/Scripts/WeedGeneration.cs
@@ -9,13 +9,12 @@ public class WeedGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("GenerateNewWeed", 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        InvokeRepeating("GenerateNewWeed", 1f, 1f);
     }
 
     void GenerateNewWeed()
@@ -25,7 +24,10 @@ public class WeedGeneration : MonoBehaviour
             Vector2Int pos = new Vector2Int(Random.Range(0, 56), Random.Range(0, 56));
             if (heatMap.GetPixel(pos.x, pos.y).r > Random.Range(0, 100))
             {
-                Instantiate(weedAsset, new Vector3(pos.x * 0.32f - 9.6f, pos.y * 0.32f - 9.6f, 0f), Quaternion.identity);            }
+                Instantiate(weedAsset, new Vector3(pos.x * 0.32f - 9.6f, pos.y * 0.32f - 9.6f, 0f), Quaternion.identity);
+            }
+            if (heatMap.GetPixel(pos.x, pos.y).r == 0)
+                break;
         }
     }
 
