37ecf1efe9743e44c13c0b0a57ce0316e2566bff
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-01-12 23:36:07 -0500

    AAJAJSDJASP

--- CompeJeux_Equipe_E/Assets/Assets/Enemies/Enemy.cs
+++ CompeJeux_Equipe_E/Assets/Assets/Enemies/Enemy.cs
@@ -7,6 +7,9 @@ public class Enemy : MonoBehaviour
 {
     public Grid grid;
     public Tilemap droplayer;
+    public Tile Seed1;
+    public Tile Seed2;
+    public Tile Seed3;
     public float Health
     {
         set
@@ -27,6 +30,16 @@ public class Enemy : MonoBehaviour
     public void Start()
     {
         grid = FindObjectOfType<Grid>();
+        droplayer = GameObject.FindGameObjectWithTag("DropLayer").GetComponent<Tilemap>();
+        Vector3 pos = transform.position;
+        Vector3Int cellPos = grid.WorldToCell(pos);
+        float random = Random.Range(0, 100);
+        if (random < 40)
+            droplayer.SetTile(cellPos, Seed1);
+        if(40<random && random<85)
+            droplayer.SetTile(cellPos, Seed2);
+        if(85< random)
+            droplayer.SetTile(cellPos, Seed3);
         health = Random.Range(5, 16);
     }
 }
