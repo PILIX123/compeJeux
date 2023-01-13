e94c06c3ca7f47b4b88ef1bc439d3aee2fa90d17
MaxB <maximebouchard2002@hotmail.com>
2023-01-13 03:55:03 -0500

    sounds synced wit animations

--- CompeJeux_Equipe_E/Assets/Assets/Player/PlayerControls.cs
+++ CompeJeux_Equipe_E/Assets/Assets/Player/PlayerControls.cs
@@ -72,8 +72,6 @@ public class PlayerControls : MonoBehaviour
                     if (!TryMove(new Vector2(movementInput.x, 0)))
                         TryMove(new Vector2(0, movementInput.y));
                 animator.SetBool("isMoving", true);
-                if(!audioSource.isPlaying)
-                    audioSource.PlayOneShot(stepClip);
                 if(!LeafParticles.isPlaying)
                     LeafParticles.Play();
             } else
@@ -168,7 +166,6 @@ public class PlayerControls : MonoBehaviour
     }
     void ScytheAttack()
     {
-        audioSource.PlayOneShot(scytheClip);
         if(spriteRenderer.flipX == true)
         {
             scytheAttack.AttackLeft();
@@ -180,7 +177,6 @@ public class PlayerControls : MonoBehaviour
     }
     void ShovelAttack()
     {
-        audioSource.PlayOneShot(shovelClip);
         if (spriteRenderer.flipX == true)
         {
             shovelAttack.AttackLeft();
@@ -192,7 +188,6 @@ public class PlayerControls : MonoBehaviour
     }
     void ShearsAttack()
     {
-        ShearsSound();
         if (spriteRenderer.flipX == true)
         {
             shearsAttack.AttackLeft();
@@ -201,11 +196,26 @@ public class PlayerControls : MonoBehaviour
         {
             shearsAttack.AttackRight();
         }
-        Invoke("ShearsSound", 0.3f);
     }
+
+    void ShovelSound()
+    {
+        audioSource.PlayOneShot(shovelClip);
+    }
+
     void ShearsSound()
     {
         audioSource.PlayOneShot(shearsClip);
+    }
+
+    void ScytheSound()
+    {
+        audioSource.PlayOneShot(scytheClip);
+    }
+
+    void StepSound()
+    {
+        audioSource.PlayOneShot(stepClip);
     }
 
 }
